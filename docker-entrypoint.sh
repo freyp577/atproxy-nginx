3a611e357884de7c5b0ca17e7cc1716830a61208
freyp567 <freyp567@googlemail.com>
2016-03-19 19:03:18 +0100

    added color to differentiate prod/staging

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -7,7 +7,12 @@ function updateconf {
 updateconf production.conf /etc/nginx/conf.d/
 updateconf staging.conf /etc/nginx/conf.d/
 
+echo $color >/usr/share/nginx/html/bluegreen.txt 
+cat << EOF > /usr/share/nginx/html/bluegreen.json
+{"active": "$color", "started": "`date '+%Y-%m-%dT%H:%M:%S'`"} 
+EOF
+
 exec "$@"
 
-#sleep infinity
+#sleep 
 
