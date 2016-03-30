40dff76183720f30e7b1c165e1067633b4a2b7d3
freyp567 <freyp567@googlemail.com>
2016-03-30 10:33:23 +0200

    output info to docker log to see why bluegreen.txt is not set at startup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,15 +4,18 @@ function updateconf {
   j2 /templates/$1.j2 > $2/$1 
 }
 
+echo update nginx conf for $color
 updateconf production.conf /etc/nginx/conf.d/
 updateconf staging.conf /etc/nginx/conf.d/
 
+echo update bluegreen.txt for $color 
 echo $color >/usr/share/nginx/html/bluegreen.txt 
+
+echo update bluegreen.txt for $color `date '+%Y-%m-%dT%H:%M:%S'`
 cat << EOF > /usr/share/nginx/html/bluegreen.json
 {"active": "$color", "started": "`date '+%Y-%m-%dT%H:%M:%S'`"} 
 EOF
 
+echo run "$@"
 exec "$@"
-
-#sleep 
 
