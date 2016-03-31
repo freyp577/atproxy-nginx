2f55d108369d182fcf71b3a53301f8b91889afad
freyp567 <freyp567@googlemail.com>
2016-03-31 14:03:05 +0200

    have bluegreen status in subdir, in order not to mix it up with baustelle

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,11 +15,12 @@ echo update nginx conf for $color
 updateconf production.conf /etc/nginx/conf.d/
 updateconf staging.conf /etc/nginx/conf.d/
 
+mkdir -p /usr/share/nginx/html/status
 echo update bluegreen.txt for $color 
-echo $color >/usr/share/nginx/html/bluegreen.txt 
+echo $color >/usr/share/nginx/html/status/bluegreen.txt 
 
 echo update bluegreen.txt for $color `date '+%Y-%m-%dT%H:%M:%S'`
-cat << EOF > /usr/share/nginx/html/bluegreen.json
+cat << EOF > /usr/share/nginx/html/status/bluegreen.json
 {"active": "$color", "started": "`date '+%Y-%m-%dT%H:%M:%S'`"} 
 EOF
 
